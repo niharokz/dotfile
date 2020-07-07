d17c0ea4d759d93e31a0a858b201ac1517d06df5
nihar <mail@nihars.com>
2020-07-07 23:57:13 +0200

    new chnage xinit

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:size=10" };
-static const char dmenufont[]       = "Hack:size=10";
+static const char *fonts[]          = { "Firacode:size=11" };
+static const char dmenufont[]       = "Firacode:size=11";
 static const char col_black[]       = "#000000";
 static const char col_white[]       = "#FFFFFF";
 static const char col_greendark[]       = "#1B8B56";
